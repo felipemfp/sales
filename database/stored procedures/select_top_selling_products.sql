73c947b7370a2557f5ebbd24ba05da8b38abca72
felipemfp <felipemfpontes@gmail.com>
2016-02-16 13:56:44 -0300

    Fix top selling products

--- database/stored procedures/select_top_selling_products.sql
+++ database/stored procedures/select_top_selling_products.sql
@@ -7,11 +7,11 @@ begin
 	declare
 		@DateStartOfMonth datetime
 	set @DateStartOfMonth = dateadd(month, datediff(month, 0, getdate()), 0)
-	select top(@Length) P.Id, P.ManufacturerId, P.Description, P.Stock, P.Price
+	select top(@Length) P.Id, P.ManufacturerId, P.Description, P.Stock, P.Price, SUM(SP.Quantity) as Sales
 	from SaleProduct SP
 		inner join Sale S on SP.SaleId = S.Id
 		right join Product P on SP.ProductId = P.Id
 	where S.DateSale >= @DateStartOfMonth
 	group by P.Id, P.ManufacturerId, P.Description, P.Stock, P.Price
-	order by COUNT(P.Id) desc
+	order by Sales desc
 end
