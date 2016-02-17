44d9f2bd4141d81f163d9d0a041acd8216cdb2f1
felipemfp <felipemfpontes@gmail.com>
2016-02-16 23:13:22 -0300

    Add wpf application project

---
+++ Sales/Sales.WPFApp/APIService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.WPFApp
+{
+    class APIService
+    {
+        private static string _baseAddress = "http://localhost:62114/api/";
+
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(_baseAddress);
+
+            return client;
+        }
+    }
+}
---
+++ Sales/Sales.WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Sales.WPFApp.Models;
+
+namespace Sales.WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitDataGrid();
+        }
+
+        async void InitDataGrid()
+        {
+            HttpResponseMessage response = await APIService.GetClient().GetAsync("clients");
+            string json = response.Content.ReadAsStringAsync().Result;
+            dataGrid.ItemsSource = JsonConvert.DeserializeObject<List<Client>>(json);
+            dataGrid.IsReadOnly = true;
+        }
+    }
+}
