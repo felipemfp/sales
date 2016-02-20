473258f25c596e77c5279ad190be89191bce2bc0
felipemfp <felipemfpontes@gmail.com>
2016-02-20 18:02:45 -0300

    Add constraint ON DELETE CASCADE

--- database/create.sql
+++ database/create.sql
@@ -9,7 +9,7 @@ create table Manufacturer (
 
 create table Product (
   Id int primary key identity,
-  ManufacturerId int not null foreign key references Manufacturer(Id),
+  ManufacturerId int not null foreign key references Manufacturer(Id) on delete cascade,
   Description varchar(100) not null,
   Stock int not null default 0,
   Price money not null
@@ -23,7 +23,7 @@ create table Client (
 
 create table Sale (
   Id int primary key identity,
-  ClientId int not null foreign key references Client(Id),
+  ClientId int not null foreign key references Client(Id) on delete cascade,
   DateSale datetime not null default getdate(),
   Total money not null default 0,
   Discount money not null default 0,
@@ -31,9 +31,9 @@ create table Sale (
 )
 
 create table SaleProduct (
-  SaleId int foreign key references Sale(Id),
+  SaleId int foreign key references Sale(Id) on delete cascade,
   ItemId int,
-  ProductId int not null foreign key references Product(Id),
+  ProductId int not null foreign key references Product(Id) on delete cascade,
   Quantity int not null,
   Price money not null,
   primary key (SaleId, ItemId)
